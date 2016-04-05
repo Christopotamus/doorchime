5512a444ee390327a0d8ece241e3c575d3a9f95b
Christopher Budd <Chris.cyborg@gmail.com>
2016-04-04 19:43:59 -0700

    Adde the player code

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var Gpio = require('onoff').Gpio,
     button = new Gpio(7, 'in', 'both');
 
+var player = require('play-sound')(opts={});
+
+var playing = false;
+
 function exit(){
   button.unexport();
   process.exit();
@@ -10,8 +14,12 @@ button.watch(function(err, value){
   if(err){
     throw err;
   }
-
-  console.log("Button changed:", value);
+  if(value === 1 && playing === false){
+      playing = true;
+      player.play('./audio/woo.ogg', function(err){
+        playing = false;
+      });
+  }
 });
 
 process.on('SIGINT', exit);
